7d57eaedac9c627e7ce6631fcbdacad75c5f00dd
Tsundere-Musume <maharjanelvis55@gmail.com>
2025-10-27 10:37:20 -0500

    217: contains duplicate - base point

---
+++ nc_150/217_contains-duplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+		HashSet<Integer> dup = new HashSet<>();
+		for (int num : nums){
+			if (dup.contains(num)){
+				return true;
+			}else{
+				dup.add(num);
+			}
+		}
+		return false;
+    }
+}
---
+++ nc_150/README.md
@@ -0,0 +1,4 @@
+# Neetcode 150
+
+- [contains-duplicate](https://leetcode.com/problems/contains-duplicate/description/)
+
